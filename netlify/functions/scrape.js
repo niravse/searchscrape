1554022d0c2a8fba7cf05aedfc271f0deaa2d8de
niravse <102810226+niravse@users.noreply.github.com>
2024-08-01 17:49:43 +0400

    Update scrape.js

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -9,7 +9,10 @@ exports.handler = async function (event, context) {
 
     const response = await axios.get(url,{
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Access-Control-Allow-Origin': '*', // Allow all origins or specify your domain
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
       }
     });
     const html = response.data;
