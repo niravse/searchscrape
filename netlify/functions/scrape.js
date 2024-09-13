948353fa753b70190a2626b69672b171e2391c52
niravse <102810226+niravse@users.noreply.github.com>
2024-09-13 18:22:48 +0400

    Update scrape.js

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -9,20 +9,21 @@ exports.handler = async function (event, context) {
       headers: {
         'Access-Control-Allow-Origin': '*', // Allow requests from any origin
         'Access-Control-Allow-Headers': 'Content-Type, Authorization', // Allow specific headers
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',// Allow specific methods
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS', // Allow specific methods
       },
-      body: '' // Preflight requests usually don't require a body
+      body: '', // Preflight requests usually don't require a body
     };
   }
 
   try {
     const searchQuery = event.queryStringParameters.searchQuery || '';
     const page = event.queryStringParameters.page || 0;
-    const url = https://www.google.com/search?vet=10ahUKEwitmdOphOOGAxUy1gIHHV9qA2MQ06ACCOkM..i&ei=FGFwZrqbA7CL7NYPh5W98Ao&opi=89978449&rlz=1C1EJFC&yv=3&rciv=jb&nfpr=0&q=${searchQuery}&start=${page}0&asearch=jb_list&cs=1&async=_id:VoQFxe,_pms:hts,_fmt:pc;
+    const url = `https://www.google.com/search?vet=10ahUKEwitmdOphOOGAxUy1gIHHV9qA2MQ06ACCOkM..i&ei=FGFwZrqbA7CL7NYPh5W98Ao&opi=89978449&rlz=1C1EJFC&yv=3&rciv=jb&nfpr=0&q=${searchQuery}&start=${page}0&asearch=jb_list&cs=1&async=_id:VoQFxe,_pms:hts,_fmt:pc`;
 
     const response = await axios.get(url, {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'X-Forwarded-For': '23.23.23.23', // Explicitly set X-Forwarded-For to 23.23.23.23
       }
     });
 
@@ -82,7 +83,7 @@ exports.handler = async function (event, context) {
     return {
       statusCode: 200,
       headers: {
-        'Access-Control-Allow-Origin': '*',// Allow requests from any origin
+        'Access-Control-Allow-Origin': '*', // Allow requests from any origin
       },
       body: JSON.stringify({
         numofLinks,
