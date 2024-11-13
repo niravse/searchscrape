6c02e4483b9f7f25dcaa3c16992d523be12a6939
niravse <102810226+niravse@users.noreply.github.com>
2024-11-13 19:05:28 +0400

    Update scrape.js

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -23,6 +23,7 @@ exports.handler = async function (event, context) {
     const response = await axios.get(url, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        'X-Forwarded-For': '203.0.113.0'
       }
     });
 
