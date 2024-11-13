7a61214d6d39d48e1b5f8a1fe89d2088b7fc556e
niravse <102810226+niravse@users.noreply.github.com>
2024-11-13 20:03:11 +0400

    Update scrape.js

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -18,13 +18,11 @@ exports.handler = async function (event, context) {
   try {
     const searchQuery = event.queryStringParameters.searchQuery || '';
     const page = event.queryStringParameters.page || 0;
-    const ip = event.queryStringParameters.page || '';
     const url = `https://www.google.com/search?vet=10ahUKEwitmdOphOOGAxUy1gIHHV9qA2MQ06ACCOkM..i&ei=FGFwZrqbA7CL7NYPh5W98Ao&opi=89978449&rlz=1C1EJFC&yv=3&rciv=jb&nfpr=0&q=${searchQuery}&start=${page}0&asearch=jb_list&cs=1&async=_id:VoQFxe,_pms:hts,_fmt:pc`;
 
     const response = await axios.get(url, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'X-Forwarded-For': ip
       }
     });
 
