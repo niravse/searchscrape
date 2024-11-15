e40b055fb4596f4cdcb95e5c30c101ce2c3b125c
niravse <102810226+niravse@users.noreply.github.com>
2024-11-15 09:15:00 +0400

    Update scrape.js

--- netlify/functions/scrape.js
+++ netlify/functions/scrape.js
@@ -63,7 +63,7 @@ exports.handler = async function (event, context) {
       descriptions.push($(elem).text().trim());
     });
     let tempLocation = [];
-    $('div.tJ9zfc').each((i, elem) => {
+    $('div.wHYlTd MKCbgd a3jPc').each((i, elem) => {
       tempLocation.push($(elem).text().trim());
     });
     let companies = [];
